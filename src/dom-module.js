6f37c69e31772abeab12eaad5e76e140e858ad33
AliHassanL12 <liaqqahl@gmail.com>
2025-03-25 13:03:46 +0000

    Create and style header application bar

--- src/dom-module.js
+++ src/dom-module.js
@@ -1,3 +1,5 @@
+import '../css/styles.css';
+import listIconPath from '../images/list.png';
 class UserInterface {
     static createUserInterface() {
         createUserInterface();
@@ -5,13 +7,19 @@ class UserInterface {
 }
 
 function createUserInterface() {
+    createAppHeader();
+}
+
+function createAppHeader() {
     const appContainer = document.querySelector('.container');
     const header = document.createElement('div');
     header.classList.add('header');
+    header.textContent = "TO-DO LIST";
+    const listIconImage = document.createElement('img');
+    listIconImage.src = listIconPath;
+    listIconImage.classList.add('listIcon');
     appContainer.appendChild(header);
-
-
-    console.log('works');
+    header.appendChild(listIconImage);
 }
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ToDoList } from "./to-do-module";
 import { Project } from "./project-module";
 import { UserInterface } from './dom-module.js';
+
 const defaultProject = new Project('default');
 const toDo = new ToDoList('Finish Dissertation', 'On optional module', new Date(2025, 6, 21), 'low');
 defaultProject.addToProject(toDo);
